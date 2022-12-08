9f6d0aa18c479a5e3ece04ece13f70ee229b1bcc
Christian Ani <iamchristianani@gmail.com>
2022-12-08 15:06:42 +0100

    Delete all completed todo list

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import './css/style.css';
-import { inputList } from './modules/variableList.js';
-import { displayList, addList } from './modules/showTask.js';
+import { inputList, clearButton } from './modules/variableList.js';
+import { displayList, addList, removeList, editTask, checkCompleted, changeTaskBg, clearCompleted } from './modules/showTask.js';
+import {allList} from './modules/variableList.js';
+
+const eventFunctions = (e) => {
+  const {target} = e;
+  const parentElement = target.parentNode;
+  if (!parentElement.classList.contains('each-list')) return;
+  const eachListId = Number(parentElement.id);
+  // target the data action
+  const {action} = target.dataset;
+
+  if (action === 'delete') {
+    removeList(eachListId);
+  } else if (action === 'checkbox') {
+    checkCompleted(eachListId, target);
+  }
+}
 
 window.addEventListener('load', () => {
   displayList();
@@ -11,3 +27,43 @@ inputList.addEventListener('keypress', (e) => {
     addList();
   }
 });
+
+allList.addEventListener('click', (e) => {
+  eventFunctions(e);
+});
+
+allList.addEventListener('change', (e) => {
+  eventFunctions(e);
+})
+
+clearButton.addEventListener('click', () => {
+  clearCompleted();
+})
+
+allList.addEventListener('focusout', (e) => {
+  const {target} = e;
+  const parentElement = target.parentNode;
+  if (!parentElement.classList.contains('each-list')) return;
+  const eachListId = Number(parentElement.id);
+
+  // target the data action
+  const {action} = target.dataset;
+  if (action === 'edit') {
+    editTask(eachListId, target);
+  }
+  eventFunctions(e);
+})
+
+allList.addEventListener('focusin', (e) => {
+  const {target} = e;
+  const parentElement = target.parentNode;
+  if (!parentElement.classList.contains('each-list')) return;
+  const eachListId = Number(parentElement.id);
+
+  // target the data action
+  const {action} = target.dataset;
+  if (action === 'edit') {
+    changeTaskBg(target);
+  }
+  // eventFunctions(e);
+})
--- src/modules/showTask.js
+++ src/modules/showTask.js
@@ -1,106 +1,80 @@
-import { allList, inputList, clearButton } from './variableList.js';
+import { allList, inputList} from './variableList.js';
 
 let tasksArr = [];
 
 const updateList = () => {
   // ADD LIST
   allList.innerHTML = '';
-  tasksArr.forEach((task) => {
+  tasksArr.forEach((task, index) => {
     const oneTask = document.createElement('li');
     oneTask.classList = 'each-list all-box';
+    oneTask.id = index;
     oneTask.innerHTML = `
-      <input data-id="${task.index}" class="input-check" type="checkbox"/>
-      <input data-id="${task.index}" type="text" id="input-text" class="input-text" value="${task.description}" />
-      <i data-id="${task.index}" class="fa-solid fa-trash list-icon" id="delete-btn"></i>
+      <input data-action="checkbox" data-id="${task.index}" class="input-check" type="checkbox"/>
+      <input data-action="edit" data-id="${task.index}" type="text" id="input-text" class="input-text" value="${task.description}" />
+      <i data-action="delete" data-id="${task.index}" class="fa-solid fa-trash list-icon" id="delete-btn"></i>
     `;
     allList.appendChild(oneTask);
     inputList.value = '';
   });
-
-  // REMOVE LIST
-  const removeListBtn = document.querySelectorAll('#delete-btn');
-  removeListBtn.forEach((button) => {
-    button.addEventListener('click', () => {
-      const dataSet = parseInt(button.dataset.id, 10);
-      const buttonId = tasksArr.findIndex((object) => object.index === dataSet);
-
-      /** REMOVE FUNCTION */
-      const removeList = (index) => {
-        tasksArr.splice(index, 1);
-        for (let i = 0; i < tasksArr.length; i += 1) {
-          tasksArr[i].index = i + 1;
-        }
-        updateList();
-        const jsonData = JSON.stringify(tasksArr);
-        localStorage.setItem('lists', jsonData);
-      };
-      removeList(buttonId);
-    });
-  });
-
-  // EDIT LIST
-  const inputText = document.querySelectorAll('#input-text');
-  inputText.forEach((input) => {
-    input.addEventListener('focusout', () => {
-      const dataSet = parseInt(input.dataset.id, 10);
-      const inputId = tasksArr.findIndex((object) => object.index === dataSet);
-      tasksArr[inputId].description = input.value;
-
-      /** EDIT FUNCTION */
-      const editTask = () => {
-        updateList();
-        const jsonData = JSON.stringify(tasksArr);
-        localStorage.setItem('lists', jsonData);
-      };
-      editTask();
-    });
-    input.addEventListener('focusin', () => {
-      input.parentElement.style.backgroundColor = '#fffed7';
-      input.style.backgroundColor = '#fffed7';
-    });
-  });
-
-  // CHECKBOX IN LIST
-  const checkBox = document.querySelectorAll('.input-check');
-  checkBox.forEach((box) => {
-    const dataSet = parseInt(box.dataset.id, 10);
-    const buttonId = tasksArr.findIndex((object) => object.index === dataSet);
-    box.addEventListener('change', () => {
-      box.nextElementSibling.classList.toggle('input-strike');
-
-      /** CHECKBOX FUNCTION */
-      if (box.checked) {
-        tasksArr[buttonId].completed = true;
-      } else {
-        tasksArr[buttonId].completed = false;
-      }
-      const isComplete = () => {
-        const jsonData = JSON.stringify(tasksArr);
-        localStorage.setItem('lists', jsonData);
-        //console.log('clicked');
-      };
-      isComplete();
-    });
-    if (tasksArr[buttonId].completed === true) {
-      box.checked = true;
-      box.nextElementSibling.classList.add('input-strike');
-    }
-  });
-
-  // CLEAR COMPLETED
-  // clearButton.addEventListener('click', () => {
-  //   for (let i = 0; i < tasksArr.length; i += 1) {
-  //     console.log(tasksArr[i].description + ' was clicked');
-  //     if (tasksArr[i].completed === true) {
-  //       const removeCheckedList = () => {
-  //         const index = tasksArr[i].index - 1;
-  //         tasksArr.splice(index, 1);
-  //       }
-  //       removeCheckedList();
-  //     }
-  //   }
-  // });
 };
+
+/** REMOVE FUNCTION */
+const removeList = (index) => {
+  tasksArr.splice(index, 1);
+  resetIndex(tasksArr);
+  updateList();
+  saveToDir(tasksArr);
+};
+
+/** EDIT FUNCTION */
+const editTask = (inputId, input) => {
+  tasksArr[inputId].description = input.value;
+  updateList();
+  saveToDir(tasksArr);
+};
+
+/** CHANGE BG WHEN EDITING */
+const changeTaskBg = (input) => {
+  input.parentElement.style.backgroundColor = '#fffed7';
+  input.style.backgroundColor = '#fffed7';
+};
+
+/** CHECKBOX FUNCTION */
+const checkCompleted = (buttonId, box) => {
+  box.nextElementSibling.classList.toggle('input-strike');
+  if (box.checked) {
+    tasksArr[buttonId].completed = true;
+  } else {
+    tasksArr[buttonId].completed = false;
+  }
+  saveToDir(tasksArr);
+  if (tasksArr[buttonId].completed === true) {
+    box.checked = true;
+    box.nextElementSibling.classList.add('input-strike');
+  }
+}
+
+// CLEAR COMPLETED TASK FUNCTION
+const clearCompleted = () => {
+  tasksArr = tasksArr.filter((obj) => obj.completed !== true);
+  updateList();
+  resetIndex(tasksArr);
+  saveToDir(tasksArr);
+}
+
+// RESET THE INDEXES
+const resetIndex = (arr) => {
+  for (let i = 0; i < arr.length; i += 1) {
+    arr[i].index = i + 1;
+  }
+}
+
+// SAVE TO LOCAL DIRECTORY
+const saveToDir = (arr) => {
+  const jsonData = JSON.stringify(arr);
+  localStorage.setItem('lists', jsonData);
+}
 
 const displayList = () => {
   const getJsonData = localStorage.getItem('lists');
@@ -117,8 +91,7 @@ const addList = () => {
   eachList.index = tasksArr.length + 1;
   tasksArr.push(eachList);
   updateList();
-  const jsonData = JSON.stringify(tasksArr);
-  localStorage.setItem('lists', jsonData);
+  saveToDir(tasksArr);
 };
 
-export { displayList, updateList, addList };
+export { displayList, updateList, addList, removeList, editTask, checkCompleted, changeTaskBg, clearCompleted };
